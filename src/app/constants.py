cc3f30d582e50d912283f8c331d90301662a5f02
Lukas Krencey <lukas.krencey@predu.sk>
2024-06-16 12:51:47 +0200

    removed some TODOs

--- src/app/constants.py
+++ src/app/constants.py
@@ -1,2 +1,1 @@
-# TODO: get url dynamically
 DATA_URL_DOWNLOAD = "https://services6.arcgis.com/fUWVlHWZNxUvTUh8/arcgis/rest/services/Events/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson"  # noqa: E501
